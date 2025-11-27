6baea5eaee83cbba23bc33b3c9de6f93f2c143d7
Edric <554639616@qq.com>
2025-11-27 22:12:23 +0800

    fix: 降级依赖版本以兼容 Snack 环境
    - 降级 React Navigation 从 v7 到 v6
    - 降级 Expo SDK 从 54 到 51
    - 降级 React 从 19.1.0 到 18.2.0
    - 添加 react-native-gesture-handler 依赖
    - 更新 babel 和 metro 配置
    - 禁用 newArchEnabled 以兼容 Snack

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, Platform, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
